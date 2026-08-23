2d478807868f1aee1be47e541f3dd1bd95337572
frogro <frank.grootens@gmail.com>
2026-08-24 01:31:58 +0200

    Install board DTB during VyOS system image updates

--- tools/assemble-board-image.sh
+++ tools/assemble-board-image.sh
@@ -146,6 +146,7 @@ KVM_USERSPACE_INSTALLER="$ROOT/tools/install-kvm-userspace.sh"
 ARM_CPU_OPMODE_PATCHER="$ROOT/tools/patch-vyos-arm-cpu-opmode.py"
 GRUB_CONSOLE_TOOL="$ROOT/tools/set-grub-console-default.py"
 GRUB_BOARD_DTB_PATCHER="$ROOT/tools/patch-vyos-grub-board-dtb.py"
+SYSTEM_IMAGE_DTB_PATCHER="$ROOT/tools/patch-vyos-system-image-dtb.py"
 
 [[ -x "$INSTALL_PROVIDER" ]] ||
     die "firmware provider installer missing: $INSTALL_PROVIDER"
@@ -166,6 +167,9 @@ fi
 
 [[ -x "$GRUB_BOARD_DTB_PATCHER" ]] ||
     die "GRUB board-DTB patcher missing: $GRUB_BOARD_DTB_PATCHER"
+
+[[ -x "$SYSTEM_IMAGE_DTB_PATCHER" ]] ||
+    die "system-image DTB patcher missing: $SYSTEM_IMAGE_DTB_PATCHER"
 
 DTB="$KERNEL_ARTIFACTS/dtb/$BOOT_FDT_FILE"
 
@@ -529,6 +533,12 @@ echo "===== ADDING BOARD DTB TO VYOS GRUB TEMPLATE ====="
 python3 "$GRUB_BOARD_DTB_PATCHER" \
     "$SQUASH_ROOT" \
     "$BOOT_FDT_FILE"
+
+echo
+echo "===== ADDING BOARD DTB SUPPORT TO VYOS SYSTEM IMAGE UPDATES ====="
+
+python3 "$SYSTEM_IMAGE_DTB_PATCHER" \
+    "$SQUASH_ROOT"
 
 echo
 echo "===== BUILDING MATCHING VYOS INITRAMFS ====="
