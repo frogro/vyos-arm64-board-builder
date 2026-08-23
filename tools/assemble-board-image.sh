11888c4d10cfe564961534958ed52302790384f9
frogro <frank.grootens@gmail.com>
2026-08-24 00:53:39 +0200

    Persist board DTB in VyOS GRUB version template

--- tools/assemble-board-image.sh
+++ tools/assemble-board-image.sh
@@ -145,6 +145,7 @@ COMMON_ROOTFS_FINALIZER="$ROOT/tools/finalize-vyos-rootfs.sh"
 KVM_USERSPACE_INSTALLER="$ROOT/tools/install-kvm-userspace.sh"
 ARM_CPU_OPMODE_PATCHER="$ROOT/tools/patch-vyos-arm-cpu-opmode.py"
 GRUB_CONSOLE_TOOL="$ROOT/tools/set-grub-console-default.py"
+GRUB_BOARD_DTB_PATCHER="$ROOT/tools/patch-vyos-grub-board-dtb.py"
 
 [[ -x "$INSTALL_PROVIDER" ]] ||
     die "firmware provider installer missing: $INSTALL_PROVIDER"
@@ -162,6 +163,9 @@ fi
 
 [[ -x "$GRUB_CONSOLE_TOOL" ]] ||
     die "GRUB console-default tool missing: $GRUB_CONSOLE_TOOL"
+
+[[ -x "$GRUB_BOARD_DTB_PATCHER" ]] ||
+    die "GRUB board-DTB patcher missing: $GRUB_BOARD_DTB_PATCHER"
 
 DTB="$KERNEL_ARTIFACTS/dtb/$BOOT_FDT_FILE"
 
@@ -518,6 +522,13 @@ echo
 echo "===== ADDING GENERIC ARM CPU DISPLAY SUPPORT ====="
 
 python3 "$ARM_CPU_OPMODE_PATCHER" "$SQUASH_ROOT"
+
+echo
+echo "===== ADDING BOARD DTB TO VYOS GRUB TEMPLATE ====="
+
+python3 "$GRUB_BOARD_DTB_PATCHER" \
+    "$SQUASH_ROOT" \
+    "$BOOT_FDT_FILE"
 
 echo
 echo "===== BUILDING MATCHING VYOS INITRAMFS ====="
